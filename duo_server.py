05643f1cd5b5f80eb959fb0973a6750574763544
Pfuenzle <dark.leon64@gmail.com>
2022-05-23 11:13:17 +0200

    Added httphandler, healthcheck, Dockerfile cleanup

---
+++ duo_server.py
@@ -0,0 +1,40 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import sys
+
+hostName = "0.0.0.0"
+serverPort = 7000
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if(self.path == "/duo_user_info.json"):
+            self.send_response(200)
+            self.send_header("Content-type", "text/json")
+            self.end_headers()
+            with open('duo_user_info.json', 'rb') as file:
+                self.wfile.write(file.read())
+        elif(self.path == "/health"):
+            self.send_response(418)
+            self.send_header("Content-type", "beverage/tee")
+            self.end_headers()
+        else:
+            self.send_response(403)
+            self.end_headers()
+
+def runServer():
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
+
+
+logfile = open("duoserver.log","w")
+sys.stdout = logfile
+sys.stdin = logfile
+sys.stderr = logfile
+
+runServer()
